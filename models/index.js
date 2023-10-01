332c15e49ec73d88615c329561e70fe6bf8c4c67
Tom Fusco <smokerdog57@gmail.com>
2023-10-01 12:24:49 -0400

    v0.6 updated ./models/index.js

--- models/index.js
+++ models/index.js
@@ -1,12 +1,11 @@
+// collect all the individual Sequelize models representing database tables) and export them as a single object 
+//so that they can be easily imported and used in other parts of the application.
+
 // Import models
 import Product from './Product';
 import Category from './Category';
 import Tag from './Tag';
 import ProductTag from './ProductTag';
 
-export default {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
-};
+// export models
+export { Product, Category, Tag, ProductTag };
